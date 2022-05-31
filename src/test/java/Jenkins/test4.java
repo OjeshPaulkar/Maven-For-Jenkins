4c605ba9c7ca1de75769d7a6d88b2f185ff0e2a2
OjeshPaulkar <ojeshpaulkar@gmail.com>
2022-05-31 09:05:12 +0530

    update for jenkins

---
+++ src/test/java/Jenkins/test4.java
@@ -0,0 +1,13 @@
+package Jenkins;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class test4 {
+	
+	@Test
+	public void test5() {
+		Reporter.log("ruby", true);
+	}
+
+}
