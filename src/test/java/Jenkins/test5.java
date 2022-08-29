f47993203c34f887efc6be548a3f73dc01b5d540
OJESH <OJESH@DESKTOP-EUI8GBJ>
2022-08-29 18:06:31 +0530

    test5 class commit

---
+++ src/test/java/Jenkins/test5.java
@@ -0,0 +1,14 @@
+package Jenkins;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class test5 {
+	
+	@Test
+	public void Csharp() {
+		Reporter.log("Hello German", true);
+	}
+
+
+}
