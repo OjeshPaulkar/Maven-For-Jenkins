f91d7b5fc87817aca7df33223faf4dda9ae97745
OJESH <OJESH@DESKTOP-EUI8GBJ>
2022-08-30 10:35:42 +0530

    test6 class commit

---
+++ src/test/java/Jenkins/test6.java
@@ -0,0 +1,13 @@
+package Jenkins;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class test6 {
+	
+	@Test
+	public void JavaScript() {
+		Reporter.log("Hello JavaScript", true);
+	}
+
+}
